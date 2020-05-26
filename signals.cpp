eded1eecb21c6a4baa29ab1b5941d06b44f77873
Gil16 <33487553+Gil16@users.noreply.github.com>
2020-05-26 17:06:40 +0300

    Signals Handlers added

--- signals.cpp
+++ signals.cpp
@@ -6,14 +6,26 @@
 using namespace std;
 
 void ctrlZHandler(int sig_num) {
-	// TODO: Add your implementation
+	cout << "smash: got ctrl-Z" << endl;
+	SmallShell& smash = SmallShell::getInstance();
+	JobsList::JobEntry* foreground = smash.m_pJobsList->getForegroundJob();
+	if(!foreground) {
+		return;
+	}
+	smash.m_pJobsList->addJob(foreground->sCommand, foreground->nId, foreground->status); // add to the jobs list
+	kill(foreground->PID, SIGSTOP);	
+	smash.m_pJobsList->updateJobstatusByPID(JobsList::EJobStatus::eJobStatus_Stopped, foreground->PID); // stopped
+	cout << "smash: process " << foreground->PID << " was stopped" << endl;
 }
 
 void ctrlCHandler(int sig_num) {
-  // TODO: Add your implementation
-}
-
-void alarmHandler(int sig_num) {
-  // TODO: Add your implementation
+	cout << "smash: got ctrl-C" << endl;
+	SmallShell& smash = SmallShell::getInstance();
+	JobsList::JobEntry* foreground = smash.m_pJobsList->getForegroundJob();
+	if(!foreground) {
+		return;
+	}
+	kill(foreground->PID, SIGKILL);	
+	cout << "smash: process " << foreground->PID << " was killed" << endl;
 }
 
