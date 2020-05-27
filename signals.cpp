bb01b09cd4edbdd42aa1ee402d3b4387026aa2bb
Gil16 <33487553+Gil16@users.noreply.github.com>
2020-05-27 16:52:42 +0300

    Minor fix

--- signals.cpp
+++ signals.cpp
@@ -8,24 +8,26 @@ using namespace std;
 void ctrlZHandler(int sig_num) {
 	cout << "smash: got ctrl-Z" << endl;
 	SmallShell& smash = SmallShell::getInstance();
-	JobsList::JobEntry* foreground = smash.m_pJobsList->getForegroundJob();
+	JobEntry* foreground = smash.m_pForeground;
 	if(!foreground) {
 		return;
 	}
-	smash.m_pJobsList->addJob(foreground->sCommand, foreground->nId, foreground->status); // add to the jobs list
 	kill(foreground->PID, SIGSTOP);	
-	smash.m_pJobsList->updateJobstatusByPID(JobsList::EJobStatus::eJobStatus_Stopped, foreground->PID); // stopped
+	smash.m_pJobsList->addJob(foreground->sCommand, foreground->PID, EJobStatus::eJobStatus_Stopped);
 	cout << "smash: process " << foreground->PID << " was stopped" << endl;
+	smash.m_pForeground = nullptr;
 }
 
 void ctrlCHandler(int sig_num) {
 	cout << "smash: got ctrl-C" << endl;
 	SmallShell& smash = SmallShell::getInstance();
-	JobsList::JobEntry* foreground = smash.m_pJobsList->getForegroundJob();
+	JobEntry* foreground = smash.m_pForeground;
 	if(!foreground) {
 		return;
 	}
 	kill(foreground->PID, SIGKILL);	
 	cout << "smash: process " << foreground->PID << " was killed" << endl;
+	smash.m_pJobsList->removeJobByPID(foreground->PID);
+	delete smash.m_pForeground;
+	smash.m_pForeground = nullptr;
 }
-
